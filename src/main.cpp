adb6894ae050ed641dd1b8083a71f51db20fbd37
Omar Al Haj <omaralhaj24@gmail.com>
2023-02-22 19:05:42 +0200

    First requirement

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
 
-int main(int argc, char *argv[])
+int getSum(const std::vector <int>& vec){
+    int sum = 0;
+
+    for(int i = 0; i < (int)vec.size(); i++){
+        sum += vec[i];
+    }
+
+    return sum;
+}
+
+int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    std::vector <int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::cout << getSum(vec) << std::endl;
 }
