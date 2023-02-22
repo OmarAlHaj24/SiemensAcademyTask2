288be862f0cb861e7e36e35fdf7eb6fe28a54670
Omar Al Haj <omaralhaj24@gmail.com>
2023-02-22 19:26:15 +0200

    Second Branch : First commit

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,21 @@ int getSum(const std::vector <int>& vec){
     return sum;
 }
 
+int getMin(const std::vector <int>& vec){
+    int min = vec[0];
+
+    for(int i = 0; i < (int)vec.size(); i++){
+        if(vec[i] < min){
+            min = vec[i];
+        }
+    }
+
+    return min;
+}
+
 int main()
 {
     std::vector <int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     std::cout << getSum(vec) << std::endl;
+    std::cout << getMin(vec) << std::endl;
 }
